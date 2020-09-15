11eb630c872698fc2198118fb6d9fca628275f68
Antoni Remeseiro <unreal.sense@gmail.com>
2020-09-15 15:13:10 +0900

    - renamed ios example app

--- iOS Example/Sources/ViewController.swift
+++ iOS Example/Sources/ViewController.swift
@@ -13,8 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view. YOUR_POLYGON_API_KEY
-        let polygon = PolygonioSwift.Client(key: "***REMOVED***")
+        // Do any additional setup after loading the view.
+        let polygon = PolygonioSwift.Client(key: "YOUR_POLYGON_API_KEY")
         
         // Market Status
         polygon.marketStatus { (result: MarketStatusResponse?, err) in
